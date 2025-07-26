9ca41d25a9f7e5d83f692ab283bd567875113093
retro98boy <retro98boy@qq.com>
2025-07-26 22:14:13 +0800

    CAINIAO CNIoT-CORE: add internal speaker support and boost U-Boot version

--- board/batocera/amlogic/s922x/cniot-core/build-uboot.sh
+++ board/batocera/amlogic/s922x/cniot-core/build-uboot.sh
@@ -5,12 +5,12 @@ BOARD_DIR=$2
 IMAGES_DIR=$3
 
 # Download mainline U-Boot source
-wget -O u-boot-2024.01.tar.gz "https://github.com/u-boot/u-boot/archive/refs/tags/v2024.01.tar.gz"
-tar zxvf u-boot-2024.01.tar.gz
-cd u-boot-2024.01
+wget -O u-boot-2025.04.tar.gz "https://github.com/u-boot/u-boot/archive/refs/tags/v2025.04.tar.gz"
+tar zxvf u-boot-2025.04.tar.gz
+cd u-boot-2025.04
 
 # Apply patches
-PATCHES="${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/amlogic/s922x/patches/uboot/*.patch"
+PATCHES="${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/amlogic/s922x/patches/u-boot-2025.04/*.patch"
 for patch in $PATCHES
 do
     echo "Applying patch: $patch"
@@ -22,39 +22,46 @@ make cainiao-cniot-core_defconfig
 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc)
 cd ..
 
+# Fetch aml_encrypt_g12b
+git clone "https://github.com/LibreELEC/amlogic-boot-fip.git"
+AML_ENCRYPT="amlogic-boot-fip/khadas-vim3/aml_encrypt_g12b"
+
 # Fetch gxlimg source then compile it
-git clone https://github.com/repk/gxlimg.git
+git clone "https://github.com/repk/gxlimg.git"
 cd gxlimg
-make
+make -j$(nproc)
 cd ..
 
 # Fetch device vendor FIP
-git clone -b blobs https://github.com/retro98boy/cainiao-cniot-core-linux.git
+git clone "https://github.com/retro98boy/amlogic-fip-blobs.git"
 
 # repack vendor FIP with mainlien u-boot.bin
-BLOBS_DIR=cainiao-cniot-core-linux
-EXTRACT_DIR="$BLOBS_DIR"/extract
-
-mkdir "$EXTRACT_DIR"
-# gxlimg returns a non-zero value upon successful extraction, causing the Armbian build to fail. Adding "|| true" forces it to return zero.
-./gxlimg/gxlimg -e "$BLOBS_DIR"/DDR.USB "$EXTRACT_DIR" || true
+BLOBS_DIR=amlogic-fip-blobs/cainiao-cniot-core
+EXTRACT_DIR="${BLOBS_DIR}/extract"
 
 mkdir -p ../uboot-cniot-core
-rm -f "$EXTRACT_DIR"/bl33.enc
-./gxlimg/gxlimg -t bl3x -s u-boot-2024.01/u-boot.bin "$EXTRACT_DIR"/bl33.enc
+rm -f "${EXTRACT_DIR}/bl33.enc"
+# The current version of gxlimg has a problem with the handling of bl3x,
+# which may cause the produced fip to fail to boot.
+# see https://github.com/repk/gxlimg/issues/19
+# ./gxlimg/gxlimg -t bl3x -s u-boot-2025.04/u-boot.bin "${EXTRACT_DIR}/bl33.enc"
+"$AML_ENCRYPT" --bl3sig \
+    --input u-boot-2025.04/u-boot.bin \
+    --output "${EXTRACT_DIR}/bl33.enc" \
+    --level v3 --type bl33
 ./gxlimg/gxlimg \
     -t fip \
-    --bl2 "$EXTRACT_DIR"/bl2.sign \
-    --ddrfw "$EXTRACT_DIR"/ddr4_1d.fw \
-    --ddrfw "$EXTRACT_DIR"/ddr4_2d.fw \
-    --ddrfw "$EXTRACT_DIR"/ddr3_1d.fw \
-    --ddrfw "$EXTRACT_DIR"/piei.fw \
-    --ddrfw "$EXTRACT_DIR"/lpddr4_1d.fw \
-    --ddrfw "$EXTRACT_DIR"/lpddr4_2d.fw \
-    --ddrfw "$EXTRACT_DIR"/diag_lpddr4.fw \
-    --ddrfw "$EXTRACT_DIR"/aml_ddr.fw \
-    --ddrfw "$EXTRACT_DIR"/lpddr3_1d.fw \
-    --bl30 "$EXTRACT_DIR"/bl30.enc \
-    --bl31 "$EXTRACT_DIR"/bl31.enc \
-    --bl33 "$EXTRACT_DIR"/bl33.enc \
+    --bl2 "${EXTRACT_DIR}/bl2.sign" \
+    --ddrfw "${EXTRACT_DIR}/ddr4_1d.fw" \
+    --ddrfw "${EXTRACT_DIR}/ddr4_2d.fw" \
+    --ddrfw "${EXTRACT_DIR}/ddr3_1d.fw" \
+    --ddrfw "${EXTRACT_DIR}/piei.fw" \
+    --ddrfw "${EXTRACT_DIR}/lpddr4_1d.fw" \
+    --ddrfw "${EXTRACT_DIR}/lpddr4_2d.fw" \
+    --ddrfw "${EXTRACT_DIR}/diag_lpddr4.fw" \
+    --ddrfw "${EXTRACT_DIR}/aml_ddr.fw" \
+    --ddrfw "${EXTRACT_DIR}/lpddr3_1d.fw" \
+    --bl30 "${EXTRACT_DIR}/bl30.enc" \
+    --bl31 "${EXTRACT_DIR}/bl31.enc" \
+    --bl33 "${EXTRACT_DIR}/bl33.enc" \
     --rev v3 ../uboot-cniot-core/u-boot.bin
