3150fd62a17f109d1a2ecf5ca4a0c7417cdb28df
retro98boy <retro98boy@qq.com>
2026-01-09 21:43:21 +0800

    rufomaculata for downstream boards

--- board/batocera/rockchip/rk3399/tn3399-v3/create-boot-script.sh
+++ board/batocera/rockchip/rk3399/tn3399-v3/create-boot-script.sh
@@ -20,6 +20,7 @@ mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
 cp "${BINARIES_DIR}/Image"                 "${BATOCERA_BINARIES_DIR}/boot/boot/linux"                || exit 1
 cp "${BINARIES_DIR}/initrd.lz4"             "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"            || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs"       "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
+cp "${BINARIES_DIR}/rufomaculata"    "${BATOCERA_BINARIES_DIR}/boot/boot/rufomaculata.update" || exit 1
 
 cp "${BINARIES_DIR}/rk3399-tn3399-v3.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
 cp "${BINARIES_DIR}/rk3399-tn3399-v3-with-1024x600-single-8bit-lvds-panel.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
