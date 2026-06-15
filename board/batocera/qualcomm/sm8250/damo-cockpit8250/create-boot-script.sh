6a115aa8eeff98ffab9427d004367f1e01d6bbee
retro98boy <retro98boy@qq.com>
2026-06-15 21:35:09 +0800

    DAMO Cockpit8250: rename cockpit8250 to damo-cockpit8250

--- board/batocera/qualcomm/sm8250/damo-cockpit8250/create-boot-script.sh
+++ board/batocera/qualcomm/sm8250/damo-cockpit8250/create-boot-script.sh
@@ -16,9 +16,9 @@ BATOCERA_BINARIES_DIR=$6
 
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" || exit 1
 
-cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/Image"           || exit 1
-cp "${BINARIES_DIR}/initrd.lz4"      "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"      || exit 1
-cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/Image"               || exit 1
+cp "${BINARIES_DIR}/initrd.lz4"      "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"          || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"     || exit 1
 cp "${BINARIES_DIR}/rufomaculata"    "${BATOCERA_BINARIES_DIR}/boot/boot/rufomaculata.update" || exit 1
 
 cp "${BINARIES_DIR}/sm8250-damo-cockpit8250.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"        || exit 1
